7c97c75c3f074ea17a9fc4f34563784c61645976
Pavel Shukhman <taleodor@gmail.com>
2022-12-22 15:50:59 -0500

    chore: move inst cert commands under cd command

--- cmd/cd.go
+++ cmd/cd.go
@@ -30,16 +30,27 @@ import (
 )
 
 var (
-	artDigest string
+	artDigest  string
+	sealedCert string
 )
 
 func init() {
+	rootCmd.AddCommand(cdCmd)
+
 	artifactGetSecrets.PersistentFlags().StringVar(&instance, "instance", "", "UUID of instance for which to generate (either this, or instanceuri must be provided)")
 	artifactGetSecrets.PersistentFlags().StringVar(&instanceURI, "instanceuri", "", "URI of instance for which to generate (either this, or instanceuri must be provided)")
 	artifactGetSecrets.PersistentFlags().StringVar(&artDigest, "artdigest", "", "Digest or hash of the artifact to resolve secrets for")
 
-	rootCmd.AddCommand(cdCmd)
 	cdCmd.AddCommand(artifactGetSecrets)
+
+	isInstHasSecretCertCmd.PersistentFlags().StringVar(&instance, "instance", "", "UUID of instance for which export from (optional)")
+	isInstHasSecretCertCmd.PersistentFlags().StringVar(&instanceURI, "instanceuri", "", "URI of instance for which to export from (optional)")
+	cdCmd.AddCommand(isInstHasSecretCertCmd)
+
+	setInstSecretCertCmd.PersistentFlags().StringVar(&instance, "instance", "", "UUID of instance for which export from (optional)")
+	setInstSecretCertCmd.PersistentFlags().StringVar(&instanceURI, "instanceuri", "", "URI of instance for which to export from (optional)")
+	setInstSecretCertCmd.PersistentFlags().StringVar(&sealedCert, "cert", "", "Sealed certificate used by the instance (required)")
+	cdCmd.AddCommand(setInstSecretCertCmd)
 }
 
 var cdCmd = &cobra.Command{
@@ -100,6 +111,77 @@ var artifactGetSecrets = &cobra.Command{
 		}
 
 		fmt.Println(string(respJson))
+	},
+}
+
+var isInstHasSecretCertCmd = &cobra.Command{
+	Use:   "iscertinit",
+	Short: "Use to check whether instance has sealed cert property configured",
+	Long: `Bitnami Sealed Certificate property is used to encrypt secrets for instance.
+	This command checks whether this property is configured for the particular instance.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var respData IsHasCertRHResp
+		client := graphql.NewClient(relizaHubUri + "/graphql")
+		req := graphql.NewRequest(`
+			query ($instanceUuid: ID, $instanceUri: String) {
+				isInstanceHasSealedSecretCert(instanceUuid: $instanceUuid, instanceUri: $instanceUri)
+			}
+		`)
+		req.Var("instanceUuid", instance)
+		req.Var("instanceUri", instanceURI)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("User-Agent", "Reliza CLI")
+		req.Header.Set("Accept-Encoding", "gzip, deflate")
+
+		if len(apiKeyId) > 0 && len(apiKey) > 0 {
+			auth := base64.StdEncoding.EncodeToString([]byte(apiKeyId + ":" + apiKey))
+			req.Header.Add("Authorization", "Basic "+auth)
+		}
+
+		if err := client.Run(context.Background(), req, &respData); err != nil {
+			printGqlError(err)
+			os.Exit(1)
+		}
+
+		jsonResp, _ := json.Marshal(respData.Responsewrapper)
+		fmt.Println(string(jsonResp))
+	},
+}
+
+var setInstSecretCertCmd = &cobra.Command{
+	Use:   "setsecretcert",
+	Short: "Use to to set sealed cert property on the instance",
+	Long: `Bitnami Sealed Certificate property is used to encrypt secrets for instance.
+	This command sets this certificate for the particular instance.
+	Only supports instance own API Key.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var respData SetCertRHResp
+		client := graphql.NewClient(relizaHubUri + "/graphql")
+		req := graphql.NewRequest(`
+			mutation ($instanceUuid: ID, $instanceUri: String, $sealedCert: String!) {
+				setInstanceSealedSecretCert(instanceUuid: $instanceUuid, instanceUri: $instanceUri,
+					sealedCert: $sealedCert)
+			}
+		`)
+		req.Var("instanceUuid", instance)
+		req.Var("instanceUri", instanceURI)
+		req.Var("sealedCert", sealedCert)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("User-Agent", "Reliza CLI")
+		req.Header.Set("Accept-Encoding", "gzip, deflate")
+
+		if len(apiKeyId) > 0 && len(apiKey) > 0 {
+			auth := base64.StdEncoding.EncodeToString([]byte(apiKeyId + ":" + apiKey))
+			req.Header.Add("Authorization", "Basic "+auth)
+		}
+
+		if err := client.Run(context.Background(), req, &respData); err != nil {
+			printGqlError(err)
+			os.Exit(1)
+		}
+
+		jsonResp, _ := json.Marshal(respData.Responsewrapper)
+		fmt.Println(string(jsonResp))
 	},
 }
 
